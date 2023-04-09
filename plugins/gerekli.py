b1fc7f35c3ef544fc5250419a4017d3d1a308edb
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-09 21:32:07 +0300

    Update and rename pmfilter.py to gerekli.py

--- plugins/gerekli.py
+++ plugins/gerekli.py
@@ -4,7 +4,16 @@ import io
 import pyrogram
 import logging
 from functions.forcesub import handle_force_subscribe
+import logging
+from urllib.parse import quote
 
+from pyrogram import Client, emoji, filters
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
+
+from database.filters_helper import get_search_results
+
+logger = logging.getLogger(__name__)
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.types import Message
@@ -281,3 +290,100 @@ async def give_filter(client,message):
             )
         except:
             pass
+
+@Client.on_inline_query(filters.user(Config.OWNER_ID) if Config.OWNER_ID else None)
+async def answer(bot, query):
+    """Show search results for given inline query"""
+
+    if Config.AUTH_CHANNEL and not await is_subscribed(bot, query):
+        await query.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text='You have to subscribe channel',
+            switch_pm_parameter="subscribe",
+        )
+        return
+
+    results = []
+    if '|' in query.query:
+        text, file_type = query.query.split('|', maxsplit=1)
+        text = text.strip()
+        file_type = file_type.strip().lower()
+    else:
+        text = query.query.strip()
+        file_type = None
+
+    offset = int(query.offset or 0)
+    reply_markup = get_reply_markup(bot.username, query=text)
+    files, next_offset = await get_search_results(text, file_type=file_type, max_results=10, offset=offset)
+
+    for file in files:
+        results.append(
+            InlineQueryResultCachedDocument(
+                title=file.file_name,
+                document_file_id=file.file_id,
+                caption=file.caption or "",
+                description=f'Size: {size_formatter(file.file_size)}\nType: {file.file_type}',
+                reply_markup=reply_markup
+            )
+        )
+
+    if results:
+        switch_pm_text = f"{emoji.FILE_FOLDER} Results"
+        if text:
+            switch_pm_text += f" for {text}"
+
+        await query.answer(
+            results=results,
+            cache_time=cache_time,
+            switch_pm_text=switch_pm_text,
+            switch_pm_parameter="start",
+            next_offset=str(next_offset)
+        )
+    else:
+
+        switch_pm_text = f'Sonuç Yok..'
+        if text:
+            switch_pm_text += f' for "{text}"'
+
+        await query.answer(
+            results=[],
+            cache_time=cache_time,
+            switch_pm_text=switch_pm_text,
+            switch_pm_parameter="okay",
+        )
+
+
+def get_reply_markup(username, query):
+    buttons = [
+        [
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+        ]
+    ]
+    return InlineKeyboardMarkup(buttons)
+
+
+def size_formatter(size):
+    """Dosya Boyutu Getirici..."""
+
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
+
+async def is_subscribed(bot, query):
+    try:
+        user = await bot.get_chat_member(Config.AUTH_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if not user.status == 'kicked':
+            return True
+
+    return False
